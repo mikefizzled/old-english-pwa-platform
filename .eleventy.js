51de022f68dda6a3259ff142b20301f92072c127
Mike Reaney <tinytange@hotmail.com>
2026-02-12 22:21:40 +0000

    Initial commit - Eleventy docs migration

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy("docs/assets");
+  eleventyConfig.addPassthroughCopy("docs/data");
+  eleventyConfig.addPassthroughCopy("docs/images");
+  eleventyConfig.addPassthroughCopy("docs/scripts");
+  eleventyConfig.addPassthroughCopy("docs/styles");
+  eleventyConfig.addPassthroughCopy("docs/CNAME");
+
+  return {
+    dir: {
+      input: "docs",
+      output: "dist"
+    },
+
+    templateFormats: ["html"],
+    
+    htmlTemplateEngine: false,
+
+    dataTemplateEngine: false,
+  };
+};
