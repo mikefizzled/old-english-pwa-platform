abbd8b6f3fb386acf29f2eea1907b5b4df91f152
Mike Reaney <tinytange@hotmail.com>
2026-02-15 15:13:34 +0000

    Add html, css, and js linters to help tidy

--- .eleventy.js
+++ .eleventy.js
@@ -1,23 +1,23 @@
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPassthroughCopy("docs/assets");
-  eleventyConfig.addPassthroughCopy("docs/data");
-  eleventyConfig.addPassthroughCopy("docs/images");
-  eleventyConfig.addPassthroughCopy("docs/scripts");
-  eleventyConfig.addPassthroughCopy("docs/styles");
-  eleventyConfig.addPassthroughCopy("docs/CNAME");
+  eleventyConfig.addPassthroughCopy('docs/assets');
+  eleventyConfig.addPassthroughCopy('docs/data');
+  eleventyConfig.addPassthroughCopy('docs/images');
+  eleventyConfig.addPassthroughCopy('docs/scripts');
+  eleventyConfig.addPassthroughCopy('docs/styles');
+  eleventyConfig.addPassthroughCopy('docs/CNAME');
 
   return {
     dir: {
-      input: "docs",
-      output: "dist"
+      input: 'docs',
+      output: 'dist',
     },
 
-    templateFormats: ["html"],
-    
+    templateFormats: ['html', 'njk'],
+
     htmlTemplateEngine: false,
 
     dataTemplateEngine: false,
 
-    pathPrefix: "/old-english-pwa-platform/"
+    pathPrefix: '/old-english-pwa-platform/',
   };
 };
---
+++ eslint.config.cjs
@@ -0,0 +1,16 @@
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        $: "readonly",
+        jQuery: "readonly"
+      }
+    }
+  }
+];
